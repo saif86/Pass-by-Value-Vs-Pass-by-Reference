365bcbc5961fd5675ecd9d0a8d7b9b2fd5d07b1c
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 13:22:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,71 @@
+/**
+* @file main.cpp
+*
+* @brief This code compares two methods of passing data to function arguments.
+* First method: Pass-by-value
+* Second method: Pass-by-Reference
+*
+* @author Saif Ullah Ijaz
+*
+*/
 #include <iostream> 
 
-using std::cout; 
-using std::endl; 
+using std::cout;
+using std::endl;
+// FUNCTION PROTOTYPE (DECLERATION)
+/** function that squares the input by value.
+*
+* @param number The number value to be squared.
+*
+* @return Square of input.
+*/
+int squareByValue(int);
 
-int squareByValue( int );	// function prototype 
-void squareByReference( int & );	// function prototype 
+/** function that squares the input by reference.
+*
+* @param numberRef Reference of the number to be squared.
+*
+* @return void.
+*/
+void squareByReference(int &);
 
-int main() { 
-	int x = 2; 
-	int z = 4; 
+// function main begins program execution
+int main() {
+	int x = 2;
+	int z = 4;
 
 	// demonstrate squareByValue 
-	cout << "x = " << x << " before squareByValue\n"; 
-	cout << "Value returned by squareByValue: "<< squareByValue( x ) << endl; 
-	cout << "x = " << x << " after squareByValue\n" << endl; 
+	cout << "x = " << x << " before squareByValue\n";
+	cout << "Value returned by squareByValue: " << squareByValue(x) << endl;
+	cout << "x = " << x << " after squareByValue\n" << endl;
 
 	// demonstrate squareByReference 
-	cout << "z = " << z << " before squareByReference" << endl; 
-	squareByReference( z ); 
-	cout << "z = " << z << " after squareByReference" << endl; 
+	cout << "z = " << z << " before squareByReference" << endl;
+	squareByReference(z);
+	cout << "z = " << z << " after squareByReference" << endl;
 
 	system("pause");
 	return 0; // indicates successful termination 
-} // end main 
-  
-  // squareByValue multiplies number by itself, stores the 
-  // result in number and returns the new value of number 
-int squareByValue( int number ) { 
+}
+// end main 
+
+/* squareByValue multiplies number by itself, stores the
+* result in number and returns the new value of number
+*/
+int squareByValue(int number) {
 
 	return number *= number; // caller's argument not modified 
 
-} // end function squareByValue 
-  
-  // squareByReference multiplies numberRef by itself and 
-  // stores the result in the variable to which numberRef 
-  // refers in function main 
-void squareByReference( int &numberRef ) { 
+}
+// end function squareByValue 
+
+/* squareByReference multiplies numberRef by itself and
+* stores the result in the variable to which numberRef
+* refers in function main
+*/
+void squareByReference(int &numberRef) {
 
 	numberRef *= numberRef; // caller's argument modified 
 
-} // end function squareByReference
+}
+// end function squareByReference
